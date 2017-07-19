19de54517283302c7c25bea3c7559f8e46e434ed
Eva Lai <evalai.dc@gmail.com>
2017-07-19 19:49:27 -0400

    connected charts component to app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import Chart from './components/Chart.js';
 
 class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //
+  //   this.state = {
+  //
+  //   }
+  // }
+  //
+  // componentDidMount(){
+  //
+  // }
 
   render() {
     return (
       <div className="App">
           <h2>Hello World</h2>
-          
+          <Chart />
       </div>
     );
   }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class Chart extends Component {
+  constructor(props){
+    super(props);
 
-  render (
-    <div className='chart'>
+    this.state = {
 
-    </div>
-  )
+    }
+  }
 
+  componentDidMount(){
+
+  }
+  
+  render() {
+    return (
+      <div className='chart'>
+      <h3> Hello from Charts </h3>
+      </div>
+    );
+  }
 }
 
 export default Chart;
