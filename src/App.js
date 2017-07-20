4d83217f51979d1a4953488a1b977e6602a394d7
Eva Lai <evalai.dc@gmail.com>
2017-07-19 20:10:05 -0400

    called all the school years using axios

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ class App extends Component {
     super(props);
 
     this.state = {
-      school_years: []
+      school_years: [],
+      selected_year: ""
     };
   }
 
   componentDidMount(){
     axios.get(`https://data.maryland.gov/resource/7dst-j5if.json?$select=school_year`)
+      .then(res => {
+        console.log(res.data.map(obj => obj.school_year))
+        // var school_years = res.data
+      })
   }
 
   render() {
