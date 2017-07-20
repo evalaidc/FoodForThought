25ae74e524edc61a22c90e3c6ec69ede2ac8ab7a
Eva Lai <evalai.dc@gmail.com>
2017-07-19 21:19:00 -0400

    added a select feature to choose school year range

--- src/App.js
+++ src/App.js
@@ -12,19 +12,33 @@ class App extends Component {
       selected_year: ""
     };
   }
-
+// react life-cycle method to load the data before page
   componentDidMount(){
     axios.get(`https://data.maryland.gov/resource/7dst-j5if.json?$select=school_year`)
       .then(res => {
-        console.log(res.data.map(obj => obj.school_year))
-        // var school_years = res.data
+        var school_years = res.data.map(obj => obj.school_year)
+
+        this.setState({school_years})
+
       })
   }
 
+
+
   render() {
+
     return (
       <div className="App">
           <h2>Hello World</h2>
+          <p> School Year Range: <select id="myselect">
+            {this.state.school_years.map(function(range){
+              return (
+                <option value={range}> {range} </option>
+              )}
+            )}
+            </select>
+          </p>
+
           <Chart />
       </div>
     );
